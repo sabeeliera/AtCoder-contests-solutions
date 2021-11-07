c9c2c7de10401393d72841dc1322b9371450fd4a
Sabrina Haque <sabeeliera@yahoo.com>
2021-11-07 17:52:29 +0600

    A - Seismic magnitude scales

---
+++ abc221 A.cpp
@@ -0,0 +1,12 @@
+//Seismic magnitude scales
+#include <iostream>
+#include <cmath> 
+using namespace std;
+int main(){
+	int a,b;
+	cin>>a>>b;
+	int c = a-b;
+	int d = pow(32,c);
+	cout<<d;
+	return 0;
+} 
